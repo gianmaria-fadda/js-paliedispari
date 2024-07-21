d8d7b46de942fbc1baee4be6fa01c8b3c40af88b
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-21 14:30:48 +0200

    Corretto codice ma ancora da finire

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,13 +1,17 @@
  const userQuestion = prompt('Inserisci la parola "Palindroma" qui sotto');
+
+ isPalindromo(Palindromo);
  
  console.log('userQuestion', userQuestion, typeof userQuestion);
 
- function isPalindromo(Palindromo) {
-    return;
-  }
+ 
+ function isPalindromo(myPalindromo) {
+  console.log('myPalindromo', myPalindromo, typeof myPalindromo);
 
-  if (isPalindromo == userQuestion) {
-    console.log(`La parola "${userQuestion}" è un palindromo.`);
-  } else {
-    console.log(`La parola "${userQuestion}" non è un palindromo.`);
+   if (userQuestion == myPalindromo) {
+     console.log(`La parola "${userQuestion}" è un palindromo.`);
+   } else {
+     console.log(`La parola "${userQuestion}" non è un palindromo.`);
+   }
+    return;
   }
